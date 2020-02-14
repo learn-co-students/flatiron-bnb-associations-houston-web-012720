418438b83f4d4c1bac6b0ed31a10e5aa2fe8d4d6
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-02-14 17:35:16 -0600

    Done.

---
+++ bin/run.rb
@@ -0,0 +1,14 @@
+require 'require_all'
+require_relative '../config/environment'
+require_all 'app'
+
+l =Listing.create(
+         address: '123 Main Street',
+         listing_type: "private room",
+         title: "Beautiful Apartment on Main Street",
+         description: "My apartment is great. there's a bedroom. close to subway....blah blah",
+         price: 50.00,
+         neighborhood: fidi,
+         host: user
+       )
+binding.pry
---
+++ db/migrate/004_create_listings.rb
@@ -0,0 +1,14 @@
+class CreateListings < ActiveRecord::Migration[5.0]
+    def change
+        create_table :listings do |t|
+            t.string :address
+            t.string :listing_type
+            t.string :description
+            t.float :price
+            t.integer :neighborhood_id
+            t.string :title
+            t.integer :host_id
+        end
+    end
+end
+
